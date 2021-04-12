d762d7c987f47b1226e43f32da29cd1a827000a2
Sandro Martini <sandro.martini@gmail.com>
2021-04-12 20:53:45 +0200

    add license header in all sources

--- src/utils.ts
+++ src/utils.ts
@@ -13,18 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import fastify from 'fastify'
-
-const server = fastify()
-
-server.get('/', async (request, reply) => {
-  return 'Hello from Fastify and TypeScript.'
-})
-
-server.listen(8000, '0.0.0.0', (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address} ...`)
-})
